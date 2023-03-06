4e6c3565f75ad2b67078c69b403d03a6ef1fdf26
Goeran Poehner <goeran.poehner@gmail.com>
2023-03-06 19:58:52 +0100

    Fixed nginx.conf

--- nginx/prepare-nginx.sh
+++ nginx/prepare-nginx.sh
@@ -2,6 +2,6 @@
 echo ">>>>> Preparing Nginx as reverse proxy"
 ## Prepare Nginx as local reverse proxy between localhost and WSL internal network
 sudo apt update && sudo apt install nginx -y
-sudo cp configs/nginx.conf /etc/nginx
+sudo cp nginx.conf /etc/nginx
 sudo service nginx restart
 echo "<<<<< Nginx ready."
